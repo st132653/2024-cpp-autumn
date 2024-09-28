34f9605987d4847c439c2b7b2364de86420cde54
Сергей Костык <kostyk.ser2017@outlook.com>
2024-09-28 23:28:50 +0300

    HW2

--- 2024.09.27/Dz_9/Source.cpp
+++ 2024.09.27/Dz_9/Source.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	int k = 0;
-	int w = 0;
-	int a1 = 0;
-	int b1 = 0;
-	int a2 = 0;
-	int b2 = 0;
-	int a3 = 0;
-	int b3 = 0;
-	scanf_s("%d", &k);
-	scanf_s("%d", &w);
-	scanf_s("%d", &a1);
-	scanf_s("%d", &b1);
-	scanf_s("%d", &a2);
-	scanf_s("%d", &b2);
-	scanf_s("%d", &a3);
-	scanf_s("%d", &b3);
+    int k = 0;
+    int w = 0;
+    int a1 = 0;
+    int b1 = 0;
+    int a2 = 0;
+    int b2 = 0;
+    int a3 = 0;
+    int b3 = 0;
+    scanf_s("%d", &k);
+    scanf_s("%d", &w);
+    scanf_s("%d", &a1);
+    scanf_s("%d", &b1);
+    scanf_s("%d", &a2);
+    scanf_s("%d", &b2);
+    scanf_s("%d", &a3);
+    scanf_s("%d", &b3);
     if (b1 + b2 >= k && a1 + a2 <= w)
     {
         printf("YES");
---
+++ 2024.09.27/Dz_912/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	int n = 0;
+	scanf_s("%d", &n);
+	int a = 2 * (n % 5) % 5;
+	int b = (n / 5) - a / 2;
+	printf("%d %d", b, a);
+	return EXIT_SUCCESS;
+}
